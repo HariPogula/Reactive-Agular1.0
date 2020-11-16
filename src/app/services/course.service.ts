8cce99f887984a698db6b0fa63873eb91e1869a4
Hari <hariar2.0@outlook.com>
2020-11-16 11:44:12 -0700

    create course service & use of async pipe in html

---
+++ src/app/services/course.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Course } from "./../model/course";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CourseService {
+  constructor(private http: HttpClient) {}
+
+  getAllCourses(): Observable<Course[]> {
+    return (
+      this.http
+        .get<Course[]>("/api/courses")
+        // To get results after "Payload" property.
+        .pipe(map((res) => res["payload"]))
+    );
+  }
+}
