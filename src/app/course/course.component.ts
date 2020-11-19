56db997c063ab9649d5d3430dc723f8ee76e759b
Hari <hariar2.0@outlook.com>
2020-11-19 10:07:08 -0700

    View Course & lessons based on Id

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,6 +1,12 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Course } from "../model/course";
 import {
   debounceTime,
   distinctUntilChanged,
@@ -11,44 +17,56 @@ import {
   concatMap,
   switchMap,
   withLatestFrom,
-  concatAll, shareReplay, catchError
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError} from 'rxjs';
-import {Lesson} from '../model/lesson';
-
-
+  concatAll,
+  shareReplay,
+  catchError,
+} from "rxjs/operators";
+import {
+  merge,
+  fromEvent,
+  Observable,
+  concat,
+  throwError,
+  combineLatest,
+} from "rxjs";
+import { Lesson } from "../model/lesson";
+import { CourseService } from "../services/course.service";
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 @Component({
-  selector: 'course',
-  templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  selector: "course",
+  templateUrl: "./course.component.html",
+  styleUrls: ["./course.component.css"],
 })
 export class CourseComponent implements OnInit {
+  // course$: Observable<Course>;
 
-  course: Course;
+  // lessons$: Observable<Lesson[]>;
+  courseData$: Observable<CourseData>;
 
-  lessons: Lesson[];
-
-  constructor(private route: ActivatedRoute) {
-
-
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private courseSerice: CourseService
+  ) {}
 
   ngOnInit() {
-
-
-
+    const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"));
+    const course$ = this.courseSerice
+      .loadCourseById(courseId)
+      .pipe(startWith(null));
+    const lessons$ = this.courseSerice
+      .loadAllCourseLessons(courseId)
+      .pipe(startWith([]));
+    this.courseData$ = combineLatest([course$, lessons$]).pipe(
+      map(([course, lessons]) => {
+        return {
+          course,
+          lessons,
+        };
+      }),
+      tap(console.log)
+    );
   }
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
